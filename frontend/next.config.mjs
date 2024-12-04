9eb42f51e5ed2bdb3d9a3ab756e0d612e455e287
Lorenzo Andraghetti <andraghetti.l@gmail.com>
2024-12-04 12:37:02 +0100

    Fix production deployment (#5)

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/thegoodailab' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/The-Good-AI-Lab' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/The-Good-AI-Lab' : '',
   images: {
     unoptimized: true,
   },
