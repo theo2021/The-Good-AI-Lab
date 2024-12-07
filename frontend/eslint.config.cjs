7fb9b6dc36fcdfc039af88ad2039a6b22298def4
Theodoros Panagiotakopoulos <theodorospanagiotakopoulos@MacBook-Pro-som-tillhor-Theodoros.local>
2024-12-07 21:13:32 +0100

    style(prettier): Adding prettier, running linting and integrate it with eslint so that it runs automatically

--- frontend/eslint.config.cjs
+++ frontend/eslint.config.cjs
@@ -1,55 +1,51 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-const reactPlugin = require('eslint-plugin-react');
-const typescriptPlugin = require('@typescript-eslint/eslint-plugin');
-const { FlatCompat } = require('@eslint/eslintrc');
-const path = require('path');
-const eslintJs = require('@eslint/js');
+const reactPlugin = require("eslint-plugin-react");
+const typescriptPlugin = require("@typescript-eslint/eslint-plugin");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const { FlatCompat } = require("@eslint/eslintrc");
+const path = require("path");
+const eslintJs = require("@eslint/js");
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: eslintJs.configs.recommended
+  recommendedConfig: eslintJs.configs.recommended,
 });
 
 const config = [
   {
-    ignores: [
-      'out/**',
-      '.next/**',
-      'build/**',
-      'dist/**',
-      'node_modules/**'
-    ]
+    ignores: ["out/**", ".next/**", "build/**", "dist/**", "node_modules/**"],
   },
   ...compat.extends(
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'next/core-web-vitals'
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
   ),
   {
-    files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+    files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
     languageOptions: {
       parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+        ecmaVersion: "latest",
+        sourceType: "module",
       },
     },
     plugins: {
       react: reactPlugin,
-      '@typescript-eslint': typescriptPlugin,
+      "@typescript-eslint": typescriptPlugin,
     },
     rules: {
-      'no-unused-vars': 'warn',
-      'no-console': 'off',
-      'eol-last': ['error', 'always'],
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "eol-last": ["error", "always"],
     },
     settings: {
       react: {
-        version: 'detect',
+        version: "detect",
       },
     },
   },
+  eslintPluginPrettierRecommended,
 ];
 
 module.exports = config;
