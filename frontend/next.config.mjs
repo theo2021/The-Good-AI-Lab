a0e2f09c2db5ae8ef73d865ce19405d81551abd5
Theodoros Panagiotakopoulos <theodorospanagiotakopoulos@MacBook-Pro-som-tillhor-Theodoros.local>
2024-12-07 12:32:12 +0100

    ci(deploy.yml): Change the next_public_base_path

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/The-Good-AI-Lab' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/The-Good-AI-Lab' : '',
+  basePath: '',
+  assetPrefix: '',
   images: {
     unoptimized: true,
   },
