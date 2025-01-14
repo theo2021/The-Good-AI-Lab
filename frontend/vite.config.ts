5a46e0ea578be0002e16f9966d6e27bb3dc179a3
Lorenzo Andraghetti <andraghetti.l@gmail.com>
2025-01-14 13:12:56 +0100

    Fix redirects problem

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,24 +3,21 @@ import react from '@vitejs/plugin-react';
 import ViteYaml from '@modyfi/vite-plugin-yaml';
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
-  return {
-    base: './',
-    plugins: [
-      react(),
-      ViteYaml(),
-    ],
-    optimizeDeps: {
-      exclude: ['lucide-react'],
-      include: ['@modyfi/vite-plugin-yaml']
-    },
-    build: {
-      assetsDir: 'assets',
-      rollupOptions: {
-        output: {
-          assetFileNames: 'assets/[name].[hash][extname]'
-        }
+export default defineConfig({
+  plugins: [
+    react(),
+    ViteYaml(),
+  ],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+    include: ['@modyfi/vite-plugin-yaml']
+  },
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]'
       }
     }
-  };
+  }
 });
